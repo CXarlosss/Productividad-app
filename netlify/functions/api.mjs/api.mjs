b6cd39e4c371edafcf79327c77c0941512662ba6
Carlos <carlosdepet@gmail.com>
2025-03-24 09:48:54 +0100

    Api

--- netlify/functions/api.mjs/api.mjs
+++ netlify/functions/api.mjs/api.mjs
@@ -1,5 +1,20 @@
+// Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
+// @ts-ignore
+export default (request, context) => {
+  try {
+    const url = new URL(request.url)
+    const subject = url.searchParams.get('name') || 'World'
+
+    return new Response(`Hello ${subject}`)
+  } catch (error) {
+    return new Response(error.toString(), {
+      status: 500,
+    })
+  }
+}
 // @ts-nocheck
 import express, { Router } from "express";
+// @ts-ignore
 import serverless from "serverless-http";
 import cors from "cors";
 import path from "path";
@@ -19,6 +34,7 @@ const dbName = "React";
 const client = new MongoClient(URI);
 
 async function connectDB() {
+  // @ts-ignore
   if (!client.topology || client.topology.s.state !== "connected") {
     await client.connect();
   }
@@ -29,12 +45,18 @@ async function connectDB() {
 export { connectDB, ObjectId };
 
 // Importamos las rutas
-const userRoutes = require("../../productividad-app/backend/routes/user");
-const activityRoutes = require("../../productividad-app/backend/routes/userActivity");
-const habitsRoutes = require("../../productividad-app/backend/routes/habits");
-const focusRoutes = require("../../productividad-app/backend/routes/focusSessions.js");
-const recommendationsRoutes = require("../../productividad-app/backend/routes/aiRecomendations");
-const tareasRoutes = require("../../productividad-app/backend/routes/tareas.js");
+
+const userRoutes = require("../../../productividad-app/backend/routes/user");
+
+const activityRoutes = require("../../../productividad-app/backend/routes/userActivity");
+
+const habitsRoutes = require("../../../productividad-app/backend/routes/habits");
+
+const focusRoutes = require("../../../productividad-app/backend/routes/focusSessions.js");
+
+const recommendationsRoutes = require("../../../productividad-app/backend/routes/aiRecomendations");
+
+const tareasRoutes = require("../../../productividad-app/backend/routes/tareas");
 
 router.use("/api/users", userRoutes);
 router.use("/api/activity", activityRoutes);
@@ -46,6 +68,7 @@ router.use("/api/tareas", tareasRoutes);
 
 
 // Ruta para servir la página de tareas
+// @ts-ignore
 router.get("/tareas", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/tareas.html"));
 });
@@ -65,4 +88,5 @@ router.get("/tareas", (req, res) => {
 
 // 🔹 Iniciar el servidor
 
+// @ts-ignore
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
